a007c286d2e9d5a2c333d23d6b80d3e1e16f03f7
jendiamond <thejendiamond@gmail.com>
2016-12-09 18:19:16 +0000

    Add Entry scaffold

---
+++ db/migrate/20161209181726_create_entries.rb
@@ -0,0 +1,10 @@
+class CreateEntries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :entries do |t|
+      t.string :cookie
+      t.string :baker
+
+      t.timestamps
+    end
+  end
+end
